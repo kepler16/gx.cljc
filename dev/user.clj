10552fcddcdae2ad20b811636ed15044889f4335
Artem Medeusheyev <artem.medeusheyev@gmail.com>
2022-05-18 15:34:32 +0600

    prepare for cli test

--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,13 @@
 (ns user
-  (:require
-   [hyperfiddle.rcf]
-   [k16.gx.beta.core]))
+  (:require [clojure.test :as t]
+            [hyperfiddle.rcf]
+            [k16.gx.beta.core]))
 
 ;; repl loads user ns by default
 ;; init RCF
 (hyperfiddle.rcf/enable!)
+
+(defn run-k16-tests
+  "Run tests with command `clj -X:test`"
+  [_]
+  (t/run-all-tests #"k16.*"))
