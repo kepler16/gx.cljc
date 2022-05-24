fde507428d902a79ef1e230c295e4428c8626665
Alexis Vincent <mail@alexisvincent.io>
2022-05-24 16:04:28 +0100

    Force push

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -97,7 +97,7 @@ const config = {
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/keplersixteen',
               },
             ],
           },
