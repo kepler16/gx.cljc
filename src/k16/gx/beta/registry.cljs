07c1e56a004e5f5b386a40df14877686291174b3
Artem Medeusheyev <artem.medeusheyev@gmail.com>
2022-05-27 14:06:12 +0600

    cljs: graph is loading during compilation to set exports

--- src/k16/gx/beta/registry.cljs
+++ src/k16/gx/beta/registry.cljs
@@ -1,4 +1,12 @@
 (ns k16.gx.beta.registry
-  (:require-macros [k16.gx.beta.registry]))
+  (:require-macros [k16.gx.beta.registry :refer [register-graph!]])
+  (:require ["fs" :as fs]
+            [clojure.edn :as edn]))
 
-(defonce entity-registry* (atom {}))
+(defn slurp [path]
+  (.readFileSync fs path "utf8"))
+
+(defn load-graph!
+  [path]
+  (let [graph (edn/read-string (slurp path))]
+    (register-graph! graph)))
