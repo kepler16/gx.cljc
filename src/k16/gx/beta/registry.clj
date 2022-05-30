d9257cf706bdc39e15f43bbc60a06248844fde6f
Artem Medeusheyev <artem.medeusheyev@gmail.com>
2022-05-30 19:41:21 +0600

    add doc to load-graph! macro

--- src/k16/gx/beta/registry.clj
+++ src/k16/gx/beta/registry.clj
@@ -18,6 +18,8 @@
     @syms*))
 
 (defmacro load-graph!
+  "Loads graph from path. Additionally on cljs side, scans for symbols
+   and populates registry (advanced compilation support)"
   [path]
   (let [graph# (edn/read-string (slurp path))]
     (if (:ns &env)
@@ -25,7 +27,3 @@
                `(swap! registry* assoc ~(str v#) ~v#))
            '~graph#)
       `'~graph#)))
-
-
-(comment
-  (load-graph! "test/fixtures/graph.edn"))
